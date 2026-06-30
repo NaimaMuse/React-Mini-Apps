dd30079119a188f3b5bcf9f2ccd2599011fcd84b
NaimaMuse <Naciimaaahmed@gmail.com>
2026-06-30 13:31:48 +0300

    created the unit  app and its design

--- my-react-app/src/Pages/Grade.jsx
+++ my-react-app/src/Pages/Grade.jsx
@@ -1,5 +1,44 @@
+import { useState } from "react";
+
 function Grade() {
-  return <h1>Calculator Page</h1>;
+  const [score, setScore] = useState("");
+  const [grade, setGrade] = useState("F");
+
+  function calculateGrade() {
+    const marks = Number(score);
+
+    if (marks >= 90 && marks <= 100) {
+      setGrade("A");
+    } else if (marks >= 80) {
+      setGrade("B");
+    } else if (marks >= 70) {
+      setGrade("C");
+    } else if (marks >= 60) {
+      setGrade("D");
+    } else {
+      setGrade("F");
+    }
+  }
+
+  return (
+    <div>
+      <h2>Grade Calculator</h2>
+
+      <input
+        type="number"
+        placeholder="Enter your score"
+        value={score}
+        onChange={(e) => setScore(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <button onClick={calculateGrade}>Calculate Grade</button>
+
+      <h3>Grade: {grade}</h3>
+    </div>
+  );
 }
 
 export default Grade;
--- my-react-app/src/Pages/Unit.jsx
+++ my-react-app/src/Pages/Unit.jsx
@@ -1,5 +1,60 @@
+import { useState } from "react";
+
 function Unit() {
-  return <h1>Calculator Page</h1>;
+  const [miles, setMiles] = useState("");
+  const [result, setResult] = useState("");
+
+  function Calculate() {
+    if (miles === "") {
+      setResult("Miles: 0 KM");
+      return;
+    }
+
+    setResult(miles * 1.60934);
+  }
+
+  return (
+    <div className="currency-page">
+
+      <div className="currency-header">
+        <h1>Unit (Miles to KM)</h1>
+      </div>
+
+      <div className="currency-card">
+
+        <div className="currency-input-box">
+
+
+          <input
+            type="number"
+            placeholder="Enter Miles Count"
+            value={miles}
+            onChange={(e) => setMiles(e.target.value)}
+          />
+
+        </div>
+
+        <button
+          className="currency-btn"
+          onClick={Calculate}
+        >
+          Convert
+        </button>
+
+        <div className="currency-result">
+
+          <h3>Miles</h3>
+
+          <div className="currency-number">
+            {result}
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  );
 }
 
 export default Unit;
